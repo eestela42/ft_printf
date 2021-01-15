b1e852a48fc712be91a96653fb4b8827dc2d740a
Eestela <augustinestela@gmail.com>
2021-01-15 15:16:20 +0100

    norm

--- fill_tab2.c
+++ fill_tab2.c
@@ -3,13 +3,13 @@
 void	space(t_tool *tool, va_list ap)
 {
 	(void)ap;
-    tool->space = 1;
+	tool->space = 1;
 }
 
 void	preci(t_tool *tool, va_list ap)
 {
 	tool->pos++;
-	while(tool->form[tool->pos] >= '0' && tool->form[tool->pos] <= '9')
+	while (tool->form[tool->pos] >= '0' && tool->form[tool->pos] <= '9')
 	{
 		tool->preci = tool->preci * 10 + tool->form[tool->pos] - '0';
 		tool->pos++;
@@ -17,7 +17,7 @@ void	preci(t_tool *tool, va_list ap)
 	if (tool->if_preci && tool->form[tool->pos] == '*')
 	{
 		tool->secu = -1;
-		return;
+		return ;
 	}
 	else if (tool->form[tool->pos] == '*')
 	{
@@ -31,15 +31,13 @@ void	preci(t_tool *tool, va_list ap)
 void	width(t_tool *tool, va_list ap)
 {
 	(void)ap;
-
 	if (tool->if_width)
 	{
 		tool->secu = -1;
-		return;
+		return ;
 	}
-	while(tool->form[tool->pos] >= '0' && tool->form[tool->pos] <= '9')
+	while (tool->form[tool->pos] >= '0' && tool->form[tool->pos] <= '9')
 		tool->width = tool->width * 10 + tool->form[tool->pos++] - '0';
 	tool->if_width = 1;
 	tool->pos--;
 }
-
